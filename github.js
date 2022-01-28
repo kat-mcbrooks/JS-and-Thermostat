125d4f265b18b2d2f3001003b495dc5d77fdbaab
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-01-28 17:41:55 +0000

    calling remote API using the got package to request data from Github url. then method to callback the handleReceivedResponse function after the url is accessed

--- github.js
+++ github.js
@@ -7,3 +7,12 @@ const handleReceivedResponse = (response) => {
 }
 
 got('https://api.github.com/repos/sinatra/sinatra').then(handleReceivedResponse);
+// I think then() is used to callback to handleReceivedResponse so that this is not executed until after the url is visited?
+// then() method returns a promise. it takes up to 2 optionals arguments but here, it is just given one argument which i think becomes the onFulfilled value. Syntax as follows:
+// p.then(onFulfilled[, onRejected]);
+
+// p.then(value => {
+//   // fulfillment
+// }, reason => {
+//   // rejection
+// });
